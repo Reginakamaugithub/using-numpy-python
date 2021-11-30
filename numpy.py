641b780d89d4de1c57657b98d22a15ab23a61061
Reginakamaugithub <95128875+Reginakamaugithub@users.noreply.github.com>
2021-11-30 16:05:24 +0300

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,5 +1,8 @@
 #importing numpy module
 import numpy as n
-#assigning attribute arange()
+#create array using arange() function
 x=n.arange(0,20,0.5)
 print(x)
+#create array
+y=[1,2,2,0,8,7,7,8,9]
+print(y)
